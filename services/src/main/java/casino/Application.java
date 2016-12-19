6b9a872c9c49cffc3905941711bd77dfdd1efccb
Melle Hofman <mel@capone.nl>
2016-12-19 20:45:27 +0100

    Making the database stuff configurable

--- services/src/main/java/casino/Application.java
+++ services/src/main/java/casino/Application.java
@@ -1,15 +1,21 @@
 package casino;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan("casino")
+@PropertySource("file:${app.config:app-config.properties}")
 public class Application {
 
     public static void main(String[] args) {
